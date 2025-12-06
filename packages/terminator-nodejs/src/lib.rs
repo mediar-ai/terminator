e8787a9008fcf9424f9bbf86307a9c60880a6584
m13v <i@m13v.com>
2025-12-05 19:41:56 -0800

    feat: export ClusteredBoundsEntry, ClusteredFormattingResult, and ElementSource types

--- packages/terminator-nodejs/src/lib.rs
+++ packages/terminator-nodejs/src/lib.rs
@@ -12,12 +12,12 @@ pub use element::{Element, TypeTextOptions};
 pub use locator::Locator;
 pub use selector::Selector;
 pub use types::{
-    Bounds, BoundsEntry, BrowserDomElement, BrowserDomResult, ClickResult, CommandOutput,
-    Coordinates, DomBoundsEntry, FontStyle, GeminiVisionResult, HighlightHandle, InspectElement,
-    Monitor, MonitorScreenshotPair, OcrBoundsEntry, OcrElement, OcrResult, OmniparserBoundsEntry,
-    OmniparserItem, OmniparserResult, OverlayDisplayMode, PropertyLoadingMode, ScreenshotResult,
-    TextPosition, TreeBuildConfig, TreeOutputFormat, UIElementAttributes, UINode,
-    VisionBoundsEntry, VisionElement, WindowTreeResult,
+    Bounds, BoundsEntry, BrowserDomElement, BrowserDomResult, ClickResult, ClusteredBoundsEntry,
+    ClusteredFormattingResult, CommandOutput, Coordinates, DomBoundsEntry, ElementSource, FontStyle,
+    GeminiVisionResult, HighlightHandle, InspectElement, Monitor, MonitorScreenshotPair,
+    OcrBoundsEntry, OcrElement, OcrResult, OmniparserBoundsEntry, OmniparserItem, OmniparserResult,
+    OverlayDisplayMode, PropertyLoadingMode, ScreenshotResult, TextPosition, TreeBuildConfig,
+    TreeOutputFormat, UIElementAttributes, UINode, VisionBoundsEntry, VisionElement, WindowTreeResult,
 };
 pub use window_manager::{WindowInfo, WindowManager};
 
